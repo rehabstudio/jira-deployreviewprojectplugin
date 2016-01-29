212f4a746adb7adf7cdb576dea0d75fa56fb986d
Peter McConnell <me@petermcconnell.com>
2016-01-29 19:19:00 +0000

    Initial commit

---
+++ deployandreview/src/main/java/com/rehabstudio/projecttemplates/DeployReviewProjectHook.java
@@ -0,0 +1,28 @@
+package com.rehabstudio.projecttemplates;
+
+import com.atlassian.jira.blueprint.api.AddProjectHook;
+import com.atlassian.jira.blueprint.api.ConfigureData;
+import com.atlassian.jira.blueprint.api.ConfigureResponse;
+import com.atlassian.jira.blueprint.api.ValidateData;
+import com.atlassian.jira.blueprint.api.ValidateResponse;
+
+public class DeployReviewProjectHook implements AddProjectHook
+{
+	@Override
+	public ValidateResponse validate(final ValidateData validateData)
+	{
+		ValidateResponse validateResponse = ValidateResponse.create();
+		if (validateData.projectKey().equals("TEST"))
+		{
+			validateResponse.addErrorMessage("Invalid Project Key");
+		}
+		return validateResponse;
+	}
+
+	@Override
+	public ConfigureResponse configure(final ConfigureData configureData)
+	{
+		ConfigureResponse configureResponse = ConfigureResponse.create().setRedirect("/issues/");
+ 		return configureResponse;
+	}
+}
